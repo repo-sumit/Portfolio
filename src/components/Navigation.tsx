73bcdf0d95662e8c70bc24a10ef506667da996fe
Sumit <128029605+repo-sumit@users.noreply.github.com>
2025-07-19 13:57:18 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,6 @@ const Navigation = () => {
   }, []);
 
   const navItems = [
-    { label: "About", href: "#about" },
     { label: "Experience", href: "#experience" },
     { label: "Projects", href: "#projects" },
     { label: "Contact", href: "#contact" },
